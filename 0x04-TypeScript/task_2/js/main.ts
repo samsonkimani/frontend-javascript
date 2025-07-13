ab1aa2ba3fcd744dc53ea42ca86b6f03a26c3ee3
samsonkimani <samsonkimani43@gmail.com>
2025-07-13 22:45:06 +0300

    validating salary

---
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -0,0 +1,107 @@
+interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [key: string]: any; 
+}
+
+interface Directors extends Teacher {
+  numberOfReports: number;
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName: string): string;
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName.charAt(0)}. ${lastName}`;
+}
+
+
+interface StudentConstructor {
+  new (firstName: string, lastName: string): StudentClassInterface;
+}
+
+interface StudentClassInterface {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+class StudentClass implements StudentClassInterface {
+  private firstName: string;
+  private lastName: string;
+
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+    return "Currently working";
+  }
+
+  displayName(): string {
+    return this.firstName;
+  }
+}
+
+
+interface DirectorInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workDirectorTasks(): string;
+}
+
+interface TeacherInterface {
+  workFromHome(): string;
+  getCoffeeBreak(): string;
+  workTeacherTasks(): string;
+}
+
+
+class Director implements DirectorInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workDirectorTasks(): string {
+    return 'Getting to director tasks';
+  }
+}
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+    return 'Working from home';
+  }
+
+  getCoffeeBreak(): string {
+    return 'Getting a coffee break';
+  }
+
+  workTeacherTasks(): string {
+    return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: number | string): Director | Teacher {
+  let numericSalary: number;
+
+  if (typeof salary === 'number') {
+    numericSalary = salary;
+  } else {
+    // Extract number from string like '$500' or '400'
+    const parsed = parseInt(salary.replace(/[^0-9]/g, ''), 10);
+    numericSalary = isNaN(parsed) ? 0 : parsed; // Default to 0 if parsing fails
+  }
+
+  if (numericSalary < 500) {
+    return new Teacher();
+  } else {
+    return new Director();
+  }
+}
